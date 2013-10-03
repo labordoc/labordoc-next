55beb28a770245e99df70adc133aedd98251afe6
Jaime Garcia <garciallopis@iloguest.org>
2013-10-03 12:21:59 +0200

    WebSearch: improved facets compatibility with MSIE8
    Conflicts:
    	modules/webstyle/etc/templates/page.html

--- modules/webstyle/lib/invenio.js
+++ modules/webstyle/lib/invenio.js
@@ -17,6 +17,35 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
+if (!Array.prototype.map) {
+  Array.prototype.map = function(callback, thisArg) {
+    var T, A, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + " is not a function");
+    }
+    if (thisArg) {
+      T = thisArg;
+    }
+    A = new Array(len);
+    k = 0;
+    while(k < len) {
+      var kValue, mappedValue;
+      if (k in O) {
+        kValue = O[ k ];
+        mappedValue = callback.call(T, kValue, k, O);
+        A[ k ] = mappedValue;
+      }  
+      k++;
+    }
+    return A;
+  };      
+}
+
 !function ($) {
 
   "use strict"; // jshint ;_;
