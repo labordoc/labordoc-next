be77a488b0027ed8970800c6dc4e256f226c3a81
Raja Sripada <rsripada@cern.ch>
2013-12-10 10:55:26 +0100

    Rollback: Websearch: Avoid duplication of dropdown suggestions.

--- modules/websearch/lib/websearch_typeahead.js
+++ modules/websearch/lib/websearch_typeahead.js
@@ -56,9 +56,7 @@
         this.options.type = 'search';
         this.source = this.options.source;
         var subquery = query.substr(query.lastIndexOf(":") + 1);
-        var typed_itm = subquery.substr(subquery.lastIndexOf(" ") + 1) + ' ';
-        if (this.source.indexOf(typed_itm) === -1)
-          this.source = [typed_itm].concat(this.source);
+        this.source = [subquery.substr(subquery.lastIndexOf(" ") + 1) + ' '].concat(this.source);        
       }
       //this.source = $.isFunction(this.source) ? this.source() : this.source
       orig.lookup.call(this);
